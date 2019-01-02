741d64acf28b82527dd127aa89d2d4ed350604c7
derivmug <404-response@tuta.io>
2019-01-02 11:02:46 +0100

    Add login controller

---
+++ controllers/LoginViewController.php
@@ -0,0 +1,56 @@
+<?php
+
+include_once __DIR__.'/../framework/Controller.php';
+
+include_once __DIR__.'/../models/UserModel.php';
+
+class LoginViewController extends Controller {
+
+    private $user_model;
+
+    private $user;
+
+    public function render_view($view_path = null) {
+
+        $this->user_model = new UserModel();
+
+        $view_params = array();
+
+        if ($this->handle_login()) {
+
+            // Successful login
+            $view_path = __DIR__.'/../views/login/successful_view.php';
+            $view_params['user_name'] = $this->user['name'];
+
+        } else {
+
+            // Unsuccessful login
+            $view_path = __DIR__.'/../views/long/unsuccessful_view.php';
+
+        }
+
+        $view_params['title'] = 'Login View';
+
+        $this->create_view($view_path, $view_params);
+
+    }
+
+
+    private function handle_login() {
+
+        $this->user = $this->user_model->get_all_by_key_value('email', $this->params['user_email']);
+
+        if ($this->user && password_verify($this->params['user_password'], $this->user['password'])) {
+
+            $_SESSION['user_id'] = $this->user['id'];
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+
+}
