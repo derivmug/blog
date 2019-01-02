c262680d09d0ea1bbae454a1d70e9c8d9cc1bfe6
derivmug <404-response@tuta.io>
2019-01-02 13:20:52 +0100

    Add view_snippets

---
+++ views/view_snippets/login.php
@@ -0,0 +1,21 @@
+<form method="POST" action="/login">
+
+<p>
+    <label for="email">
+        <span>Email: </span>
+        <strong><abbr title="required">*</abbr></strong>
+    </label>
+    <input type="email" id="email" name="user_email" placeholder="Email">
+</p>
+
+<p>
+    <label for="password">
+        <span>Password: </span>
+        <strong><abbr title="required">*</abbr></strong>
+    </label>
+    <input type="password" id="password" name="user_password" placeholder="Password">
+</p>
+
+<button type="submit">Log in</button>
+
+</form>
