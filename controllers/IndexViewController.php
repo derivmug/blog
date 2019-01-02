1e022d621153c38047e2c4e72105e794934c6aec
derivmug <404-response@tuta.io>
2019-01-02 10:10:19 +0100

    Add doc

--- controllers/IndexViewController.php
+++ controllers/IndexViewController.php
@@ -4,6 +4,9 @@ include_once __DIR__.'/../framework/Controller.php';
 
 class IndexViewController extends Controller {
 
+    /**
+     * Renders the index_view
+     */
     public function render_view() {
 
         $view_params['title'] = 'Index View';
