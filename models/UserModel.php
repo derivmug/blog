e143b0eda70910c873bfd65c5aedb735088b0da2
derivmug <404-response@tuta.io>
2019-01-02 10:08:15 +0100

    Add doc

--- models/UserModel.php
+++ models/UserModel.php
@@ -4,6 +4,9 @@ include_once __DIR__.'/../framework/Model.php';
 
 class UserModel extends Model {
 
+    /**
+     * Class constructor, calls the constructor of Model with values specific for the User table
+     */
     function __construct() {
 
         parent::__construct('user', array('id', 'name', 'email', 'password'));
