76af87089ee7fcb83903a2e8a7155a2265ef6330
derivmug <404-response@tuta.io>
2019-01-03 13:36:10 +0100

    Cleanup articles_view

--- views/article/articles_view.php
+++ views/article/articles_view.php
@@ -5,25 +5,4 @@
     </div>
 </div>
 
-<?php foreach($view_params['articles'] as $key => $article): ?>
-
-    <div class="container m-2">
-        <div class="columns">
-
-            <div class="column col-6 col-mx-auto card my-2">
-
-                <div class="card-header">
-                    <div class="card-title h5"><?=$article['header']?></div>
-                    <div class="card-subtitle text-gray"><i>Created at: </i><?=$article['created_at']?> - <i>Written by: </i><?=$article['author_name']?></div>
-                </div>
-
-                <div class="card-body">
-                    <?=$article['content']?>
-                </div>
-
-            </div>
-
-        </div>
-    </div>
-
-<?php endforeach; ?>
+<?php require_once(__DIR__.'/../view_snippets/all_articles.php') ?>
--- views/view_snippets/all_articles.php
+++ views/view_snippets/all_articles.php
@@ -1,13 +1,13 @@
 <?php foreach($view_params['articles'] as $key => $article): ?>
 
-    <div class="container">
+    <div class="container m-2">
         <div class="columns">
 
             <div class="column col-6 col-mx-auto card my-2">
 
                 <div class="card-header">
                     <div class="card-title h5"><?=$article['header']?></div>
-                    <div class="card-subtitle text-gray"><?=$article['author_name']?></div>
+                    <div class="card-subtitle text-gray"><i>Created at: </i><?=$article['created_at']?> - <i>Written by: </i><?=$article['author_name']?></div>
                 </div>
 
                 <div class="card-body">
