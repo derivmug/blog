70edafa54f5a1b6ce33ce6250c660541de18cfd1
derivmug <404-response@tuta.io>
2019-01-03 14:12:21 +0100

    Basic user profile view

--- controllers/UserViewController.php
+++ controllers/UserViewController.php
@@ -7,11 +7,16 @@ include_once __DIR__.'/../models/UserModel.php';
 
 class UserViewController extends Controller {
 
+    private $user_model;
+
     public function render_view($view_path = null) {
+
+        $this->user_model = new UserModel();
 
         $view_path = __DIR__.'/../views/user/user_profile_view.php';
 
         $view_params['logged_in'] = isset($_SESSION['user_id']);
+        $view_params['user_name'] = $this->user_model->get_all_by_key_value('id', $_SESSION['user_id'])['name'];
 
         $this->create_view($view_path, $view_params);
 
---
+++ views/user/user_profile_view.php
@@ -0,0 +1,6 @@
+<div class="hero bg-dark">
+    <div class ="hero-body text-center">
+        <h1><i>Profile: <?=$view_params['user_name']?></i></h1>
+        <p class="text-large">The user's profile</p>
+    </div>
+</div>
